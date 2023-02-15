6876f322899a7cc12a7825d041b770368146a175
EricFormar <menaeric@hotmail.com>
2023-02-15 20:58:37 -0300

    add validations in form add product

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,3 +1,4 @@
+const { validationResult } = require("express-validator");
 const { readJSON, writeJSON } = require("../data");
 
 module.exports = {
@@ -29,25 +30,39 @@ module.exports = {
   },
   store: (req, res) => {
 
-      const products = readJSON('products.json');
-      const { name, price, category, description, discount, brand } = req.body;      
+      const errors = validationResult(req);
 
-      const newProduct = {
-        id: products.length ? products[products.length - 1].id + 1 : 1,
-        name: name.trim(),
-        price: +price,
-        discount : +discount,
-        category,
-        brand,
-        description : description.trim(),
-        image : req.file ? req.file.filename : null,
-      };
+      if(errors.isEmpty()){
+        const products = readJSON('products.json');
+        const { name, price, category, description, discount, brand } = req.body;      
+  
+        const newProduct = {
+          id: products.length ? products[products.length - 1].id + 1 : 1,
+          name: name.trim(),
+          price: +price,
+          discount : +discount,
+          category,
+          brand,
+          description : description.trim(),
+          image : req.file ? req.file.filename : null,
+        };
+  
+        products.push(newProduct);
+  
+        writeJSON('products.json', products)
+  
+        return res.redirect("/");
 
-      products.push(newProduct);
+      }else{
+         return res.render('productAdd',{
+          categories : readJSON('categories.json'),
+          brands : readJSON('brands.json'),
+          errors : errors.mapped(),
+          old : req.body
+         })
+      }
 
-      writeJSON('products.json', products)
-
-      return res.redirect("/");
+      
    
   },
   edit: (req, res) => {
